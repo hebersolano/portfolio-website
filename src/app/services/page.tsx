07bbef37ff9159e3876b9ed6c2c3d08fcbe8bd79
Heber Solano <hebersolano1@gmail.com>
2024-08-03 08:40:37 -0600

    services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,6 +2,8 @@
 
 import { motion } from "framer-motion";
 import services from "@/entities/services";
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
 
 function ServicesPage() {
   return (
@@ -17,12 +19,32 @@ function ServicesPage() {
               ease: "easeIn",
             },
           }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
         >
           {services.map((service, i) => (
-            <div key={i}>
-              <div>
-                <div>{service.num}</div>
+            <div key={i} className="flex-1 flex flex-col justify-center gap-6 group">
+              {/* top */}
+              <div className="w-full flex justify-between items-center">
+                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover translate-all duration-500">
+                  {service.num}
+                </div>
+                <Link
+                  href={service.href}
+                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                >
+                  <BsArrowDownRight className="text-3xl text-primary" />
+                </Link>
               </div>
+              {/* title */}
+              <h2
+                className="text-[42px] font-bold leading-no]
+               text-white group-hover:text-accent transition-all duration-500 "
+              >
+                {service.title}
+              </h2>
+              {/* description */}
+              <p className="text-white/60">{service.description}</p>
+              <div className="border-b border-white/20 w-full"></div>
             </div>
           ))}
         </motion.div>
