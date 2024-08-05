7db57d25be40f47fe288467fabf28a4de11d7f07
Heber Solano <hebersolano1@gmail.com>
2024-08-04 21:36:33 -0600

    resume page 0.3

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Tooltip,
@@ -7,6 +8,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { experience } from "@/entities/resume";
 import { motion } from "framer-motion";
 
 function ResumePage() {
@@ -20,7 +22,65 @@ function ResumePage() {
       className="flex min-h-[80vh] items-center justify-center py-12 xl:py-0"
     >
       <div className="container">
-        <Tabs></Tabs>
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col gap-[60px] lg:flex-row"
+        >
+          <TabsList className="mx-auto flex w-full max-w-[300px] flex-col gap-6 xl:mx-0">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div className="min-h-[70vh] w-full">
+            {/* experience */}
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="mx-auto max-w-[600px] text-white/60 xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 gap-[30px] lg:grid-cols-2">
+                    {experience.items.map((item, i) => (
+                      <li
+                        key={i}
+                        className="flex h-[184px] flex-col items-center justify-center gap-1 rounded-xl bg-[#232329] px-10 py-6 lg:items-start"
+                      >
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="min-h-[60px] max-w-[260px] text-center text-xl lg:text-left">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          {/* dot */}
+                          <span className="h-[6px] w-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            {/* education */}
+            <TabsContent value="education " className="w-full">
+              education
+            </TabsContent>
+
+            {/* skills */}
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+
+            {/* about */}
+            <TabsContent value="about" className="w-full">
+              about
+            </TabsContent>
+          </div>
+        </Tabs>
       </div>
     </motion.div>
   );
