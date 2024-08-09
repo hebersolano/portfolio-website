c0392b981b317cfd9115900d370814f7707e85cc
Heber Solano <hebersolano1@gmail.com>
2024-08-08 21:01:57 -0600

    work page 0.1

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,161 @@
+"use client";
+
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { motion } from "framer-motion";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import { useState } from "react";
+import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Quis sint occaecat sint irure laboris reprehenderit exercitation ut. Adipisicing et ex est excepteur eu deserunt.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
+    description:
+      "Quis sint occaecat sint irure laboris reprehenderit exercitation ut. Adipisicing et ex est excepteur eu deserunt.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "fullstack",
+    title: "project 2",
+    description:
+      "Quis sint occaecat sint irure laboris reprehenderit exercitation ut. Adipisicing et ex est excepteur eu deserunt.",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
+
 function WorkPage() {
-  return <div>WorkPage</div>;
+  const [project, setProject] = useState(projects[0]);
+
+  function handleSlideChange(swiper: SwiperClass) {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  }
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: {
+          delay: 2.4,
+          duration: 0.4,
+          ease: "easeIn",
+        },
+      }}
+      className="flex min-h-[80vh] flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="order-2 flex w-full flex-col xl:order-none xl:h-[460px] xl:w-[50%] xl:justify-between">
+            <div className="flex h-[40%] flex-col gap-[30px]">
+              {/* outline num */}
+              <div className="text-outline text-8xl font-extrabold leading-none text-transparent">
+                {project.num}
+              </div>
+              {/* project category */}
+              <h2 className="text-[42px] font-bold capitalize leading-none text-white transition-all duration-500 group-hover:text-accent">
+                {project.category}
+              </h2>
+              {/* project description */}
+              <p className="text-white/60">{project.description}</p>
+              {/*stack */}
+              <ul>
+                {project.stack.map((item, i) => (
+                  <li key={i} className="text-xl text-accent">
+                    {item.name}
+                    {i !== project.stack.length - 1 && ","}
+                  </li>
+                ))}
+              </ul>
+              {/* border */}
+              <div className="border border-white/20"></div>
+              {/* buttons */}
+              <div className="flex items-center gap-4">
+                {/* live project button */}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="group flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white/5">
+                        <BsArrowUpRight className="text-3xl text-white group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+
+                {/* github button */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="group flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white/5">
+                        <BsGithub className="text-3xl text-white group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[60%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              onSlideChange={handleSlideChange}
+              className="xl:h-520px mb-12"
+            >
+              {projects.map((project, i) => (
+                <SwiperSlide key={i} className="w-full">
+                  <div className="group relative flex h-[460px] items-center justify-center bg-pink-50/20">
+                    {/* overlay */}
+                    <div className=""></div>
+                    {/* image */}
+                    <div className="relative">
+                      <img
+                        src={project.image}
+                        alt="project image"
+                        className="h-full w-full object-cover"
+                      />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 
 export default WorkPage;
